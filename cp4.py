d5886a0f9e328dd5dc9cbe1581cdd0ced9332252
Pranaythumati <163623013+Pranaythumati@users.noreply.github.com>
2024-03-23 09:59:16 +0530

    Add files via upload

---
+++ cp4.py
@@ -0,0 +1,24 @@
+a=(input("enter number:"))
+
+s1=list(a)
+for i in range(len(s1)):
+    if s1[i] == 0:
+        print("zero")
+    if s1[i] == 1:
+        print("one")
+    if s1[i] == 2:
+        print("two")
+    if s1[i] == 3:
+        print("three")
+    if s1[i] == 4:
+        print("four")
+    if s1[i] == 5:
+        print("five")
+    if s1[i] == 6:
+        print("six")
+    if s1[i] == 7:
+        print("seven")
+    if s1[i] == 8:
+        print("eight")
+    if s1[i] == 9:
+        print("nine")
