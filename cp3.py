29ab6c2b7735e213a8e9a2acd0fa4d2cb37c4f35
Pranaythumati <163623013+Pranaythumati@users.noreply.github.com>
2024-03-23 09:58:40 +0530

    Add files via upload

---
+++ cp3.py
@@ -0,0 +1,17 @@
+a=input('enter word1:')
+b=input('enter word2:')
+
+def anagram():
+    
+    s1=set(a)
+    s2=set(b)
+    if s1 == s2:
+        print("true")
+    else:
+        print("false")
+    for i in range(len())
+    
+    
+
+anagram()
+
